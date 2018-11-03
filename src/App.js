e15f36002d851871fe3e51a81882466fda09073a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-03 13:29:45 +0530

    feat: Add empty place list
    Adding array of 5 empty place list.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+  state = {
+    placeList: [
+      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
+      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
+      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
+      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
+      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }} 
+    ]
+  }
 }
 
 export default App;
