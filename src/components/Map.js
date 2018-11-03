9bcd38bc692cf8c1852e7e6a94ef8c6da0798111
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-03 19:45:58 +0530

    feat: Update Map.js file

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap} from 'react-google-map';
+
+const Map = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={props.zoom}
+    defaultCenter={props.center}>
+  </GoogleMap>
+));
