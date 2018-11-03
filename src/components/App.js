db0d099f916c21b5e0534b13d03dd687044e32b5
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-03 16:18:24 +0530

    refactor: Create a components folder
    Adding the JavaScript file to component folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 class App extends Component {
   state = {
@@ -9,7 +9,7 @@ class App extends Component {
       { title: "Ranchi Lake", placeType: "natural_feature", location: {lat: 23.368364 , lng: 85.318145 }, forStreetView: {lat: 23.368364 , lng: 85.318145 }},
       { title: "Ranchi Science Centre", placeType: "museum", location: {lat: 23.406945, lng: 85.340316 }, forStreetView: {lat: 23.406945, lng: 85.340316 }},
       { title: "Shree Krishna Singh Park", placeType: "park", location: {lat: 23.336656, lng: 85.319276 }, forStreetView: {lat: 23.336656, lng: 85.319276 }},
-      { title: "Baheya Waterfall", placeType: "point_of_interest", location: {lat: 23.362652,lng: 85.491187 }, forStreetView: {lat: 23.362652,lng: 85.491187 }} 
+      { title: "Baheya Waterfall", placeType: "point_of_interest", location: {lat: 23.362652,lng: 85.491187 }, forStreetView: {lat: 23.362652,lng: 85.491187 }}
     ]
   }
 }
