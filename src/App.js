becf19e3a48c618777dd0d2b00847aed7b9c1b4f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-03 13:59:14 +0530

    fix: Update empty place list.
    Filling the required information in empty place list

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 class App extends Component {
   state = {
     placeList: [
-      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
-      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
-      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
-      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }},
-      { title: "", placeType: "", location: {lat: , lng: }, forStreetView: {lat:, lng: }} 
+      { title: "Bhagwan Birsa Biological Park", placeType: "park", location: {lat:23.46318 , lng: 85.455203 }, forStreetView: {lat:23.46318 , lng: 85.455203 }},
+      { title: "Ranchi Lake", placeType: "natural_feature", location: {lat: 23.368364 , lng: 85.318145 }, forStreetView: {lat: 23.368364 , lng: 85.318145 }},
+      { title: "Ranchi Science Centre", placeType: "museum", location: {lat: 23.406945, lng: 85.340316 }, forStreetView: {lat: 23.406945, lng: 85.340316 }},
+      { title: "Shree Krishna Singh Park", placeType: "park", location: {lat: 23.336656, lng: 85.319276 }, forStreetView: {lat: 23.336656, lng: 85.319276 }},
+      { title: "Baheya Waterfall", placeType: "point_of_interest", location: {lat: 23.362652,lng: 85.491187 }, forStreetView: {lat: 23.362652,lng: 85.491187 }} 
     ]
   }
 }
