8e6577d3bc28f72977434443a981bec9df3b1ea3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 11:37:52 +0530

    feat: Update ErrorPage.js file
    Adding content to the error page.

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = (props) => {
+  return (
+    <main className="error-page">
+      <h3 className="inner-margin">Oops ! error in retrieving data. Check your internet connection. </h3>
+    </main>
+  );
+};
+
+export default ErrorPage;
