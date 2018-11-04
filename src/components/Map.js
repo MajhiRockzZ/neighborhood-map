6861a40d0df0031a6fd6be216a70c643596dcdbd
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 11:22:39 +0530

    feat: Create marker
    Updating Map.js file

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap} from 'react-google-map';
+import Marker from './Marker';
 
 const Map = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={props.zoom}
     defaultCenter={props.center}>
+
+    {props.filteredPlaces.map((place) => {
+      return (
+        <Marker
+          key={place.title}
+          title={place.title}
+          position={place.location}
+          setClicked={props.setClicked}
+          setCurrentPlace={props.setCurrentPlace}
+          currentPlace={props.currentPlace}
+          resetFilterdPlaces={props.resetFilteredPlaces}
+          forStreetView={place.forStreetView}
+          animationConstant={props.animationConstant}
+        />
+      );
+    })}
   </GoogleMap>
 ));
+
+export default Map;
