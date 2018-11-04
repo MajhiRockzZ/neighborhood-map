d13b544878b2d04c1af95c1fa6e3ba6d31c24e5f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 19:19:36 +0530

    chore: Update npm packages

---
+++ src/components/Search.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {List, ListItem} from 'material-ui/List'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import Restaurant from 'material-ui/svg-icons/maps/restaurant'
+import LocalLibrary from 'material-ui/svg-icons/maps/local-library'
+import LocalCafe from 'material-ui/svg-icons/maps/local-cafe'
+import LocalGroceryStore from 'material-ui/svg-icons/maps/local-grocery-store'
+import BeachAccess from 'material-ui/svg-icons/places/beach-access'
+import Cake from 'material-ui/svg-icons/social/cake'
+import School from 'material-ui/svg-icons/social/school'
+
+class Search extends Components {
+  state {
+  query: '',
+ }
+
+ updateQuery = (query) => {
+
+ }
+
+
+}
