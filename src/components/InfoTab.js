019117151e1c7e0ab79aa03ceb3366cc28da9333
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 19:49:58 +0530

    feat: Adding necessary state

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -1,7 +1,33 @@
 import React, {Component} from 'react'
 import IconButton from 'material-ui/IconButton'
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
-import { List, ListItem } from 'material-ui/List'
+import {List, ListItem} from 'material-ui/List'
 import ThumbUp from 'material-ui/svg-icons/action/thumb-up'
 import ErrorPage from './ErrorPage'
-import { FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET} from "../api/APIkey";
+import {FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET} from "../api/APIkey";
+
+class InfoTab extends Component {
+  state = {
+    imageSrc: '',
+    address: '',
+    formattedAddress: '',
+    coordinates: [],
+    categories: [],
+    phoneContact: '',
+    twitterContact: '',
+    facebookContact: '',
+    url: '',
+    openStatus: '',
+    timeFrames: [],
+    popularTimes: [],
+    rating: -1.0,
+    tip: '',
+    menuUrl: '',
+    menuMobileUrl: '',
+    price: '',
+    description: '',
+    likesCount: -1,
+    location: '',
+    infoLoaded: true,
+  }
+}
