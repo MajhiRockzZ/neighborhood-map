53d55973417a6e91d5ab17a098a06f34519ebf13
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 19:48:18 +0530

    fix: Update InfoTab.js file
    Import necessary files and packages.

---
+++ src/components/InfoTab.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+import IconButton from 'material-ui/IconButton'
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+import { List, ListItem } from 'material-ui/List'
+import ThumbUp from 'material-ui/svg-icons/action/thumb-up'
+import ErrorPage from './ErrorPage'
+import { FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET} from "../api/APIkey";
