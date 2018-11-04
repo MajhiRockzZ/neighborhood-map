3cad313df4aaa468520f938bacc969cb675461b6
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 18:13:33 +0530

    feat: Update Panorama.js file
    Completed.

--- src/components/Panorama.js
+++ src/components/Panorama.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap, StreetViewPanorama} from 'react-google-maps';
 import {compose, withProps} from 'recompose';
+import {GOOGLE_MAP_API_KEY} from "../api/APIkey";
 
 const Panorama = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBULcmVQScz-PlscSwmxFocRmLBboUTfWk&v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_MAP_API_KEY + "&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{height: `100%`}}/>,
     mapElement: <div style={{height: `100%`}}/>,
     containerElement: <div style={{width: `300px`, height: `280px`}}/>
