bca84d2b5ab9be70c4ba3b9fd86aca3a74ca44ed
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 07:19:24 +0530

    feat: Render Markers

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -5,13 +5,13 @@ class Marker extends Component {
   state = {
     isOpen: false,
     animationConstant: 0,
-  }
+  };
 
   onToggleOpen = () => {
     if (this.state.isOpen && (this.props.title === this.props.currentPlaces)) {
       this.props.setClicked(false)
       this.props.setCurrentPlace('')
-      this.props.resetFilteredPlace()
+      this.props.resetFilteredPlaces()
     } else if (!this.state.isOpen) {
       this.props.setClicked(false)
       this.props.setCurrentPlace(this.props.title)
@@ -21,4 +21,25 @@ class Marker extends Component {
       isOpen: !prevState.isOpen
     }))
   }
+
+  render() {
+    return (
+      <Marker
+        key={this.props.title}
+        title={this.props.title}
+        position={this.props.position}
+        onClick={this.onToggleOpen}
+        animation={(this.props.title === this.props.currentPlace) ? this.props.animationConstant : 0}
+      >
+        {this.state.isOpen &&
+        <InfoWindow onCloseClick={this.onToggleOpen}>
+          <div role="Complementary" style={{width: `300px`, height: `300px`}}>
+            <div role="Banner" className="marker-title-style"><b>{this.props.title}</b></div>
+          </div>
+        </InfoWindow>}
+      </Marker>
+    )
+  }
 }
+
+export default Marker
