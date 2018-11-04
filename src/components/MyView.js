7455a048c6ead252e2e9828ee3cbda36c9966b79
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-04 15:31:39 +0530

    feat: Add MyViews
    Adding MyViews and exporting it.

--- src/components/MyView.js
+++ src/components/MyView.js
@@ -1,2 +1,20 @@
 import React from 'react';
-import {withScriptjs, withGoogleMap, GoogleMap, StreetViewPanorama} from 'react-google-map'
+import {withScriptjs, withGoogleMap, GoogleMap, StreetViewPanorama} from 'react-google-maps';
+import {compose, withProps} from 'recompose';
+
+const MyView = compose(
+  withProps({
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBULcmVQScz-PlscSwmxFocRmLBboUTfWk&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{height: `100%`}}/>,
+    mapElement: <div style={{height: `100%`}}/>,
+    containerElement: <div style={{width: `300px`, height: `280px`}}/>
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap defaultZoom={8} defaultCenter={props.position}>
+    <StreetViewPanorama defaultPosition={props.forStreetView} visible={}/>
+  </GoogleMap>
+);
+
+export default MyView;
