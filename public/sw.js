dc630207342ea746cb2e9bb7fb0747f7a865fbf4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-05 10:41:59 +0530

    feat: Add urls to be cache
    Update sw.js file.

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,17 @@
 let staticCacheName = 'neighborhood-map-v1';
 
+self.addEventListener('install', function (event) {
+  let urlsToCache = [
+    '/',
+    'index.html',
+    '../src/App.css',
+    '../src/index.js',
+    '../src/components/App.js',
+    '../src/components/ErrorPage.js',
+    '../src/components/InfoTab.js',
+    '../src/components/MyMap.js',
+    '../src/components/MyMarker.js',
+    '../src/components/Panorama.js',
+    '../src/components/Search.js'
+  ]
+});
